ee461e5de73841e8c2abe25dab5c684aa752876c
Bin <bin.ngoctu@gmail.com>
2022-12-12 22:06:07 +0700

    Debug Libs

--- {{cookiecutter.project_slug}}/sample_project/settings.py
+++ {{cookiecutter.project_slug}}/sample_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
+    "django_extensions",
     "{{ cookiecutter.app_slug }}",
 ]
 
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "sample_project.urls"
@@ -116,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- {{cookiecutter.project_slug}}/sample_project/urls.py
+++ {{cookiecutter.project_slug}}/sample_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("{{ cookiecutter.app_slug }}/", include("{{ cookiecutter.app_slug }}.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
