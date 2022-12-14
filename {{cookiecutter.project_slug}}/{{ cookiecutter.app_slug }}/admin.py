ec6ca0e6307189f9ea3a40f8d6e70925bd35fae2
Bin <bin.ngoctu@gmail.com>
2022-12-14 10:01:13 +0700

    Using Custom site if available

--- {{cookiecutter.project_slug}}/{{ cookiecutter.app_slug }}/admin.py
+++ {{cookiecutter.project_slug}}/{{ cookiecutter.app_slug }}/admin.py
@@ -1,2 +1,8 @@
 """Register Models for Admin Interface."""
-# from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin
+
+if "django_admin" in settings.INSTALLED_APPS:
+    from django_admin.admin import site  # noqa: F401
+else:
+    site = admin.site
